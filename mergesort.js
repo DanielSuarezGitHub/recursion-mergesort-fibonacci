b8e6e07bda75fa0fadea7393c917771ca8a516e1
Daniel Suarez <danielsuarezscripts@gmail.com>
2022-11-05 18:41:35 -0700

    merge subdivided arrays

--- mergesort.js
+++ mergesort.js
@@ -1,14 +1,30 @@
 
 function mergesort(Arr) {
     if (Arr.length == 1 ) {
-        console.log("array subdivided to its minimum");
         return Arr
     } else {
         let mid = Math.floor(Arr.length / 2)
         let arrL = Arr.slice(0, mid)
         let arrR =  Arr.slice(mid)
-        return mergesort(arrL).concat(mergesort(arrR))
+         return merge(mergesort(arrL), mergesort(arrR))
     }
 }
 
-mergesort([1,3,4,56,7,78])
+function merge(Arr1, Arr2) {
+    let length = (Arr1.length + Arr2.length)
+    let merged = []
+    while (merged.length != length) {
+        if (Arr1[0] <= Arr2[0] || Arr2.length == 0) {
+            merged.push(Arr1[0])
+            Arr1.shift()
+        } else {
+            merged.push(Arr2[0])
+            Arr2.shift()
+        }
+        
+    }
+    return merged
+}
+
+
+console.log(mergesort([4,12,2363,1,5345,12]));
